fca0106b80da090707be20491d648727443fc079
anuragrao04 <raoanu2004@gmail.com>
2023-03-26 10:04:22 +0530

    added bitwise operators

--- Sem2/class_stuff/unit-1/bitwise_operators/swapTwoNumbers.c
+++ Sem2/class_stuff/unit-1/bitwise_operators/swapTwoNumbers.c
@@ -13,24 +13,51 @@ int main() {
     a = a ^ b;
     
     printf("After swapping: a = %d, b = %d\n", a, b);
+    // imagine a = 1, b = 2
+    // in binary:
+    // a = 01
+    // b = 10 
+
+    // a = a ^ b:
+    // 01 ^ 10 = 00
+
+    // b = a ^ b:
+    // 00 ^ 10 = 01
+
+    // a = a ^ b
+    // 00 ^ 01 = 10
+
+    // a = 10 = 2
+    // b = 01 = 1
+
+
+    // USING A TEMP VARIABLE:
+    a = 0, b = 0;
+    int temp = 0;
+    printf("Now using a temp variable. \n");
+    printf("Enter two numbers to be swapped: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
     
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+
+    printf("Now without using a temp variable. \n");
+    printf("Enter two numbers to be swapped: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    a = a + b;  // a now holds the sum of the original values of a and b
+    b = a - b;  // b now holds the original value of a
+    a = a - b;  // a now holds the original value of b
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+
+
     return 0;
 }
 
 
-// imagine a = 1, b = 2
-// in binary:
-// a = 01
-// b = 10 
-
-// a = a ^ b:
-// 01 ^ 10 = 00
-
-// b = a ^ b:
-// 00 ^ 10 = 01
-
-// a = a ^ b
-// 00 ^ 01 = 10
-
-// a = 10 = 2
-// b = 01 = 1
