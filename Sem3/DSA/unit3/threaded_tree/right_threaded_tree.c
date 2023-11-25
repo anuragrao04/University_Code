5e4f1ec9344e6c1eb8599e51dab5268fc5ad0e52
anuragrao04 <raoanu2004@gmail.com>
2023-11-25 19:50:33 +0530

    fixed threaded tree

--- Sem3/DSA/unit3/threaded_tree/right_threaded_tree.c
+++ Sem3/DSA/unit3/threaded_tree/right_threaded_tree.c
@@ -3,40 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct node {
   int data;
-  struct node* left;
-  struct node* right;
+  struct node *left;
+  struct node *right;
   int rthread;
 } node_t;
 
-void insert_node(node_t** tree, int data);
-void inorder(node_t* tree);
-
+void insert_node(node_t **tree, int data);
+void inorder(node_t *tree);
 
 // helper functions
-node_t* getNode(int data);
-void setLeft(node_t* root, node_t* node);
-void setRight(node_t* root, node_t* node);
+node_t *getNode(int data);
+void setLeft(node_t *prev, node_t *node);
+void setRight(node_t *prev, node_t *node);
+node_t *leftmost(node_t *node);
 
-
-
-int main(int argc, char *argv[])
-{
-  node_t* tree = NULL;
+int main(int argc, char *argv[]) {
+  node_t *tree = NULL;
   int n;
   printf("Enter number of elements: ");
   scanf("%d", &n);
+  n = 5;
   printf("Enter elements space separated: ");
-  int elements[64];
-  for(int i = 0; i < n; i++){
+  int elements[n];
+  for (int i = 0; i < n; i++) {
     scanf("%d", &elements[i]);
   }
-  
 
   // insertion of all elements
-  for(int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++) {
     insert_node(&tree, elements[i]);
   }
 
@@ -46,15 +42,14 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
-
-node_t* getNode(int data){
-  node_t* new_node = (node_t *)malloc(sizeof(node_t));
+node_t *getNode(int data) {
+  node_t *new_node = (node_t *)malloc(sizeof(node_t));
   new_node->data = data;
-  new_node->left = new_node->right = NULL;
+  new_node->left = NULL;
+  new_node->right = NULL;
   new_node->rthread = 1;
   return new_node;
 }
-
 
 void insert_node(node_t **root, int data) {
   node_t *new_node = getNode(data);
@@ -62,56 +57,59 @@ void insert_node(node_t **root, int data) {
     *root = new_node;
     return;
   } else {
-    node_t *prev = NULL;
+    node_t *prev = *root;
     node_t *current = *root;
     while (current != NULL) {
       prev = current;
       if (data < current->data) {
         current = current->left;
+      } else if (current->rthread) {
+        break;
       } else {
         current = current->right;
       }
     }
+
     if (data < prev->data) {
       setLeft(prev, new_node);
-      
     } else {
       setRight(prev, new_node);
     }
   }
 }
 
-
-void setLeft(node_t* root, node_t* node){
-  root->left = node;
-  root->rthread = 0;
-  node->right = root;
+void setLeft(node_t *prev, node_t *node) {
+  prev->left = node;
+  node->right = prev;
   node->rthread = 1;
 }
 
-
-
-void setRight(node_t* tree, node_t* node){
-  node->right = tree;
-  node->rthread = 1;
-  tree->right = node;
-  tree->rthread = 0;
+void setRight(node_t *prev, node_t *node) {
+  node->right = prev->right;
+  prev->rthread = 0;
+  prev->right = node;
 }
 
-void inorder(node_t* tree){
-  node_t* current = tree;
-  while(current != NULL){
-    while(current->left != NULL){
-      current = current->left;
+node_t *leftmost(node_t *node) {
+  if (node == NULL) {
+    return node;
+  } else {
+    while (node->left != NULL) {
+      node = node->left;
     }
-    printf("%d ", current->data);
-    while(current->rthread == 1){
-      current = current->right;
-      if(current == NULL){
-        return;
-      }
-      printf("%d ", current->data);
-    }
-    current = current->right;
+    return node;
   }
+}
+
+void inorder(node_t *tree) {
+  node_t *current = leftmost(tree);
+  while (current != NULL) {
+    printf("%d ", current->data);
+    if (current->rthread) {
+      current = current->right;
+    } else {
+      current = leftmost(current->right);
+    }
+  }
+  printf("\n");
 }
